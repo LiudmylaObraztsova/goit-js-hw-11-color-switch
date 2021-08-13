0f930b8077da71503ab360ab7376d8f4134d056c
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-08-14 00:25:11 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import './sass/main.scss';
-import { colors } from './colors'
+import { randomIntegerFromInterval } from './random-integer';
+import { colors } from './colors';
 
 // console.log('colors[1] :>> ', colors[1]);
 
 const refs = {
     startBtn: document.querySelector('button[data-action="start"]'),
-    stopBtn:document.querySelector('button[data-action="start"]'),
+    stopBtn: document.querySelector('button[data-action="start"]'),
+    body: document.querySelector('body'),
+}
+let changeColorId;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+const newRandomColor = () =>
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+
+
+function onStartBtnClick(e) {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    changeColorId = setInterval(newRandomColor, 1000);
 }
 
+function onStopBtnClick(e) {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(changeColorId )
+}
 // console.log('refs.startBtn :>> ', refs.startBtn);
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-//  <button type="button" data-action="start">Start</button>
-// <button type="button" data-action="stop">Stop</button>
